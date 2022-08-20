d76d5c9e56e81cd4002ce98342391fc668ac3174
Tinh Truong <hi@tinhtruong.me>
2022-08-20 23:08:35 +1000

    Installed fisher plugin PatrickF1/fzf.fish

--- fish/.config/fish/conf.d/fisher.fish
+++ fish/.config/fish/conf.d/fisher.fish
@@ -1,5 +1,12 @@
 # Fisher is a fish plugin manager
-# Configure fisher data in its own folder so that it won't pollute the fish folder with plugins
+# Set fisher data to a folder outside of fish config so that it won't pollute the fish folder with plugins' functions, completions
 set -U fisher_path ~/.config/fisher
 
+# Load the fisher function
 source ~/.config/fisher/functions/fisher.fish
+
+# Load all the fisher plugin's function
+set -gx fish_function_path $fisher_path/functions $fish_function_path
+
+# Load all the fisher plugin's completion
+set -gx fish_complete_path $fisher_path/completions $fish_complete_path
