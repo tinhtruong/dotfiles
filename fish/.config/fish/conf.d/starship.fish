5be77ab8adc1a4fb6f45bc00657a3663aa7cf082
Tinh Truong <hi@tinhtruong.me>
2022-08-21 09:29:03 +1000

    Only setup starship if it's installed

--- fish/.config/fish/conf.d/starship.fish
+++ fish/.config/fish/conf.d/starship.fish
@@ -1,1 +1,8 @@
-starship init fish | source
+# Starship https://starship.rs/ integration
+if status is-interactive
+  # Check if starship was installed https://stackoverflow.com/questions/42831558/check-if-a-program-exists-from-a-fish-script
+  if command -sq starship
+    starship init fish | source
+  end
+end
+
