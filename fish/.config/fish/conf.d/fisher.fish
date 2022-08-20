a00bbfb7830ab1f75ee538ce32fc122784197d3c
Tinh Truong <hi@tinhtruong.me>
2022-08-21 09:14:21 +1000

    Only load fisher config and plugins if its config folder exists

--- fish/.config/fish/conf.d/fisher.fish
+++ fish/.config/fish/conf.d/fisher.fish
@@ -1,16 +1,21 @@
 # Fisher is a fish plugin manager
-# Set fisher data to a folder outside of fish config so that it won't pollute the fish folder with plugins' functions, completions
-set -g fisher_path ~/.config/fisher
+if status is-interactive
+    # Only setup fisher plugin manager if its config folder exists
+    if test -d ~/.config/fisher
+      # Set fisher data to a folder outside of fish config so that it won't pollute the fish folder with plugins' functions, completions
+      set -g fisher_path ~/.config/fisher
 
-# Load the fisher function
-source ~/.config/fisher/functions/fisher.fish
+      # Load the fisher function
+      source ~/.config/fisher/functions/fisher.fish
 
-# Load all the fisher plugin's function
-set -gx fish_function_path $fisher_path/functions $fish_function_path
+      # Load all the fisher plugin's function
+      set -gx fish_function_path $fisher_path/functions $fish_function_path
 
-# Load all the fisher plugin's completion
-set -gx fish_complete_path $fisher_path/completions $fish_complete_path
+      # Load all the fisher plugin's completion
+      set -gx fish_complete_path $fisher_path/completions $fish_complete_path
 
-for plugin_conf in $fisher_path/conf.d/*.fish
-    source $plugin_conf
+      for plugin_conf in $fisher_path/conf.d/*.fish
+          source $plugin_conf
+      end
+    end
 end
