ae239f041a4457e1de94f65d55dc945af6ba8a3b
Tinh Truong <hi@tinhtruong.me>
2022-08-25 22:26:58 +1000

    Integrate zoxide to fish shell

---
+++ fish/.config/fish/conf.d/zoxide.fish
@@ -0,0 +1,8 @@
+# Zoxide https://github.com/ajeetdsouza/zoxide integration
+if status is-interactive
+  # Check if zoxide was installed https://stackoverflow.com/questions/42831558/check-if-a-program-exists-from-a-fish-script
+  if command -sq zoxide
+    zoxide init fish | source
+  end
+end
+
