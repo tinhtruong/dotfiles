c6cdd3fddd42ba467976841ccf5b61204ec6c8ef
Tinh Truong <hi@tinhtruong.me>
2022-08-25 21:31:59 +1000

    Reinstalled all fisher plugins

--- fish/.config/fish/conf.d/fisher.fish
+++ fish/.config/fish/conf.d/fisher.fish
@@ -6,7 +6,7 @@ if status is-interactive
       set -g fisher_path ~/.config/fisher
 
       # Load the fisher function
-      source ~/.config/fisher/functions/fisher.fish
+      source $fisher_path/functions/fisher.fish
 
       # Load all the fisher plugin's function
       set -gx fish_function_path $fisher_path/functions $fish_function_path
@@ -16,6 +16,14 @@ if status is-interactive
 
       for plugin_conf in $fisher_path/conf.d/*.fish
           source $plugin_conf
+      end
+      
+      for plugin_func in $fisher_path/functions/*.fish
+          source $plugin_func
+      end
+      
+      for plugin_completions in $fisher_path/completions/*.fish
+          source $plugin_completions
       end
     end
 end
