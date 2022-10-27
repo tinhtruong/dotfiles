85703674dc53d76f54e3afe968c6acbf9c146c66
Tinh Truong <code@tinhtruong.me>
2022-10-28 00:08:05 +1100

    Load completions for asdf

--- fish/.config/fish/conf.d/asdf.fish
+++ fish/.config/fish/conf.d/asdf.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # set up adsf version manager
     if test -f ~/.asdf/asdf.fish
         source ~/.asdf/asdf.fish
+        set -gx fish_complete_path ~/.asdf/completions $fish_complete_path
     end
 end
