64eab854fc3c7d44237e215c52f16a5e643a0d0f
Tinh Truong <code@tinhtruong.me>
2023-08-25 12:07:19 +1000

    Added findutils integration

---
+++ fish/.config/fish/conf.d/gnu_utils.fish
@@ -0,0 +1,18 @@
+
+# GNU bash 5.1+ and GNU tools (coreutils, findutils) integration integration
+if status is-interactive
+    # Only add coreutils bin to path if exists
+    if test -d /usr/local/opt/coreutils
+        set -gp fish_user_paths /usr/local/opt/coreutils/libexec/gnubin
+    end
+    if test -d /opt/homebrew/opt/coreutils
+        set -gp fish_user_paths /opt/homebrew/opt/coreutils/libexec/gnubin
+    end
+    # Only add findutils bin to path if exists
+    if test -d /usr/local/opt/findutils
+        set -gp fish_user_paths /usr/local/opt/findutils/libexec/gnubin
+    end
+    if test -d /opt/homebrew/opt/findutils
+        set -gp fish_user_paths /opt/homebrew/opt/findutils/libexec/gnubin
+    end
+end
