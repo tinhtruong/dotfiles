f352b0cf20deb6fc9cf0609f53213243d101f9bf
Tinh Truong <code@tinhtruong.me>
2023-08-26 11:58:45 +1000

    Fixed warning reported by homebrew doctor. Rename gnu_utils to gnu_tools

--- fish/.config/fish/conf.d/homebrew.fish
+++ fish/.config/fish/conf.d/homebrew.fish
@@ -3,3 +3,7 @@ if test -d /opt/homebrew/bin
     contains /opt/homebrew/bin $fish_user_paths; or set -ga fish_user_paths /opt/homebrew/bin
     contains /opt/homebrew/sbin $fish_user_paths; or set -ga fish_user_paths /opt/homebrew/sbin
 end
+
+if test -d /usr/local/sbin
+    contains /usr/local/sbin $fish_user_paths; or set -ga fish_user_paths /usr/local/sbin
+end
