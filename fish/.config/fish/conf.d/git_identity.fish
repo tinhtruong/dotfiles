7200a4cc07382c39fee123961c4832d8604c6660
Tinh Truong <code@tinhtruong.me>
2023-09-26 10:00:32 +1000

    Fixed an issue detect added Git identities

--- fish/.config/fish/conf.d/git_identity.fish
+++ fish/.config/fish/conf.d/git_identity.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     set addedIdentity (ssh-add -L)
-    if test -z "$addedIdentity"
+
+    if string match -q "*no identities*" $addedIdentity
+        echo "There is no Git identities, adding them now..."
         ssh-add --apple-use-keychain ~/.ssh/conf.d/id_tinhtruong_github/key.priv
         ssh-add --apple-use-keychain ~/.ssh/conf.d/id_other_bitbucket/key.priv
         ssh-add --apple-use-keychain ~/.ssh/conf.d/id_other_gitlab/key.priv
