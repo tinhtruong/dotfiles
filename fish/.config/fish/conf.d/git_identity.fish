56244bed4646c8c77965fff1a7f007dcd7230ce3
Tinh Truong <code@tinhtruong.me>
2024-02-18 21:48:13 +1100

    Fixed typo

--- fish/.config/fish/conf.d/git_identity.fish
+++ fish/.config/fish/conf.d/git_identity.fish
@@ -1,8 +1,8 @@
 if status is-interactive
-    # List all SSH identities and store the output to the addedIdentities
+    # List all SSH identities and store the output to the addedIdentities variable
     set addedIdentities (ssh-add -L)
 
-    if string match -q "*no identities*" $addedIdentities
+    if string match -q "*no identities*" $addedIdentities # if the output contain the phrase'no identities'
         echo "There is no Git identities, adding them now..."
         ssh-add --apple-use-keychain ~/.ssh/conf.d/id_tinhtruong_github/key.priv
         ssh-add --apple-use-keychain ~/.ssh/conf.d/id_other_bitbucket/key.priv
