944e3ba2ee17d19437b8e180bbe39329b4e32200
Tinh Truong <code@tinhtruong.me>
2024-02-19 20:18:08 +1100

    Replace exa with eza as exa is unmaintained

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -1,8 +1,8 @@
 # Set up command aliases
 
-# https://github.com/ogham/exa
-alias ls "exa"
-alias tree "exa --tree"
+# https://github.com/eza-community/eza
+alias ls "eza"
+alias tree "eza --tree"
 
 # https://github.com/sharkdp/bat
 alias cat "bat"
