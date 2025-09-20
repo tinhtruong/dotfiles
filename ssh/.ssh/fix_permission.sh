b429cc0383b6355f41e0f79eaf2493e6eea3e3eb
Tinh Truong <code@tinhtruong.me>
2025-09-20 21:31:29 +1000

    Update script to fix file permission

--- ssh/.ssh/fix_permission.sh
+++ ssh/.ssh/fix_permission.sh
@@ -6,13 +6,13 @@
 chmod 700 ~/.ssh
 
 # Set permissions for all directories inside .ssh
-find ~/.ssh -type d -exec chmod 700 {} +
+find ~/.ssh/ -type d -exec chmod 700 {} +
 
 # Set permissions for all files inside .ssh
-find ~/.ssh -type f -exec chmod 600 {} +
+find ~/.ssh/ -type f -exec chmod 600 {} +
 
 # Relax permissions for public keys
-find ~/.ssh -name "*.pub" -exec chmod 644 {} +
+find ~/.ssh/ -name "*.pub" -exec chmod 644 {} +
 
 # Relax permissions for known_hosts
 chmod 644 ~/.ssh/known_hosts
