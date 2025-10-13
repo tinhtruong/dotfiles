b1852b41fc3c74deb310a057cd82d6d298046c85
Tinh Truong <code@tinhtruong.me>
2025-10-13 23:04:44 +1100

    chore: tweak the script so that it can run on Linux and inside container as well

--- fish/.config/fish/conf.d/git_identity.fish
+++ fish/.config/fish/conf.d/git_identity.fish
@@ -1,11 +1,25 @@
 if status is-interactive
+    # Start ssh-agent if not running
+    # ssh-agent will set the SSH_AUTH_SOCK env var when it is running
+    if not set -q SSH_AUTH_SOCK
+        eval (ssh-agent -c)
+    end
+
     # List all SSH identities and store the output to the addedIdentities variable
     set addedIdentities (ssh-add -L)
 
-    if string match -q "*no identities*" $addedIdentities # if the output contain the phrase'no identities'
+    if string match -q "*no identities*" $addedIdentities # if the output contain the phrase 'no identities'
         echo "There is no Git identities, adding them now..."
-        ssh-add --apple-use-keychain ~/.ssh/conf.d/id_tinhtruong_github/key.priv
-        ssh-add --apple-use-keychain ~/.ssh/conf.d/id_other_bitbucket/key.priv
-        ssh-add --apple-use-keychain ~/.ssh/conf.d/id_other_gitlab/key.priv
+        # Check if not running inside a Docker container
+        #if not test -f /.dockerenv
+            # If running on macOS, use macOS specific flag
+            if test (uname) = Darwin
+                set useAppleKeyChain "--apple-use-keychain"
+            end
+            ssh-add $useAppleKeyChain ~/.ssh/conf.d/id_tinhtruong_github/key.priv
+            ssh-add $useAppleKeyChain ~/.ssh/conf.d/id_other_bitbucket/key.priv
+            ssh-add $useAppleKeyChain ~/.ssh/conf.d/id_other_gitlab/key.priv
+        #end
+
     end
 end
